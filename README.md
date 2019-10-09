eec2ef06a2cdc557e08a6208d2792ea6b3993fa0
ocb-dev-04 <oscarchb04@gmail.com>
2019-10-09 01:17:42 -0400

    *Variables module is finish

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # c_sharp.tutorial.languaje
 A tutorial from 0 to Pro... in dotnet core how framework.
+
+# Introduccion
+
+
+# Variables
+A variable is used to store information in memory that we can use later.
+
+To use variables we must declare them and start them.
+
+To declare a variable explicitly we use the following syntax:
+
+type identifier;
+int varName;
+
+The type is the type of data that stores the variable, it can be an integer, character string, date, boolean, etc ...
+
+The identifier is the name of the variable
+
+# Methods
---
+++ var_declare_and_initilize/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace var_declare_and_initilize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable declaration
+            // if declare a type cannot assign other type later
+            int age;
+            char vocal;
+            string name;
+            DateTime dateNow;
+            bool isDay;
+
+            // initializing
+            age = 22;                   // integer no need '' or ""
+            vocal = 'a';                // char assign just one character inside ''
+            name = "Jhon";              // string support world or phrases
+            dateNow = DateTime.Now;     // date assign DateTime value
+            isDay = true;               // accept just true or false no more
+
+            // can declare and initilize at same time
+            int days = 10;
+            string first = "Jose";
+
+            // NOTE: if you see a green or orange line under the variables this mean that not use this varaibles
+            // not is an error but, just is a sintaxis helper
+
+            // can declare and assign multiple variables
+            string 
+                    firstName = "Tommy", 
+                    lastName = "Smith", 
+                    direcction = "Malibub, California";
+
+            Console.WriteLine($"{firstName} {lastName} life in {direcction}");
+
+            Console.ReadKey();
+        }
+    }
+}
