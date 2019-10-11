8dedbbaf346b2cef72171f90bbc6fdaa935d2138
ocb-dev-04 <oscarchb04@gmail.com>
2019-10-11 01:29:14 -0400

    *Add some files more

---
+++ 011. bloques_and_scope/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _011._bloques_and_scope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // puedo acceder a esta variable en todo este bloque de codigo del metodo main
+            string name = "jon";
+
+            var p = new Person();
+            // mediante la instancia puedo acceder a las propiedades y metodos publicos
+            p.lastName = "Santos";
+            p.PublicMethod();
+
+            // pero no puedo acceder a las propiedades y metodos privados
+            // p.Age = 55;// da error ya que el m odificador de acceso no me permite acceder a ella
+        }
+
+        // no puedo acceder a name desde aqui ya que sta fuera de bloque
+        // name = "Carl";
+
+        public void Main2() 
+        {
+            // aunque no me muestre error, dara error a la hora de compilar, ya que en este metodo no e definido una variable de nombre name
+            // y no puedo acceder desde aqui a la variable definida en el Main
+            name = "Jessica";
+        }
+
+        class Person// la clase de declara tal cual porque la usaremos en el mismo archivo, si fuera una clase exportada, hayq que definirla como public para poder acceder a ella
+        {
+            public string lastName { get; set; }
+            private int Age { get; set; }
+
+            private void PrivateMethod()
+            {
+                Console.WriteLine("Output from private method");
+            }
+
+            public void PublicMethod()
+            {
+                Console.WriteLine("Output from public method");
+            }
+        }
+    }
+}
