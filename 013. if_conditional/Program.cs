804125a64b82e55707f7edf621d4a76580861e67
ocb-dev-04 <oscarchb04@gmail.com>
2019-10-12 01:18:49 -0400

    *Add if condition file

---
+++ 013. if_conditional/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _013._if_conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Fulano";
+            string phrase = "Esta es una frase normal";
+
+            var longitudName = name.Length;
+
+            Console.WriteLine(longitudName);
+            if(longitudName != 6)// si la longitud del nombre es diferente de 6
+            {
+                Console.WriteLine(phrase.Length);
+            }
+            else if(longitudName == 6)
+            {
+                Console.WriteLine("La longitud es 6");
+            }
+            else // si es igual a 6, entonces
+            {
+                // se puede ejecutar un if dentro de otro if
+                if(longitudName > 10)
+                {
+                    Console.WriteLine("La longitud es mayr que 10");
+                }
+            }
+
+        }
+    }
+}
